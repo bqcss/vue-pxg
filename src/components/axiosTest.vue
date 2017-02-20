00250b4e7ba0c05c5ddcb7b0aa2948a26ded38fe
baruide <ba_ruide@163.com>
2017-02-20 15:31:31 +0800

    测试axios的使用
    真的只是简单的获取并且渲染。没有使用api接口，也没有状态管理

---
+++ src/components/axiosTest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+  <button @click='showmock'>点击异步获取mock接口数据</button>
+  <pre>{{pre}}</pre>
+  <div>
+    <img :src="img" alt="" v-for=' img of imgsList'>
+    {{imgfirst}}
+  </div>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios"
+import mock from '../server/mock.js'
+export default {
+  data:function(){
+    return {
+      pre:" ",
+      imgfirst:'',
+      img:'',
+      imgsList:[]
+    }
+  },
+  methods:{
+    showmock(){
+     axios.get('/index/getList')
+    .then((response) =>{
+      // console.log(response.request.response);
+     // this.pre=(JSON.parse(response.request.response).data.code)
+     // this.imgfirst=JSON.parse(response.request.response).data.data[0].imgGrounp[0]
+     this.imgsList=JSON.parse(response.request.response).data.data[0].imgGrounp
+    })
+    .catch(function (response) {
+      console.log(response);
+    });
+
+    }
+  }
+}
+</script>
